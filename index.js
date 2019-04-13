a4ca24def034294e596cde489af7d27fc3d247bb
Justis Gipson <justis710@gmail.com>
2019-04-12 23:38:28 -0400

    cleaned up for readability, added missing requires

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+
 const files = require('./lib/files');
-const inquirer = require('./lib/inquirer');
+const github = require('./lib/github');
+const repo = require('./lib/repo');
 
 clear();
 
@@ -17,14 +21,46 @@ if (files.directoryExists('.git')) {
   process.exit();
 };
 
+const getGithubToken = async () => {
+  let token = github.getStoredGithubToken();
+  if (token) {
+    return token;
+  }
+
+  await github.setGithubCredentials();
+
+  token = await github.registerNewToken();
+  return token;
+}
 
 const run = async() => {
-  let token = github.getStoredGithubToken();
-  if (!token) {
-    await github.setGithubCredentials();
-    token = await github.registerNewToken();
+  try {
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    const url = await repo.createRemoteRepo();
+
+    await repo.createGitIgnore();
+
+    const done = await repo.setupRepo(url);
+
+    if (done) {
+      console.log(chalk.green('All Done!'));
+    }
+  } catch(err) {
+    if (err) {
+      switch (err.code){
+        case 401:
+          console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+          break;
+        case 422:
+          console.log(chalk.red('Remote repository already exists with same name.'));
+          break;
+        default: 
+          console.log(err);
+      }
+    }
   }
-  console.log(token);
 }
 
 run();
